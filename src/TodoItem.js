63c390435c2fc7e87e220f9ba34e8f74bec2eeb6
sashajustice <sabrinnasreddin@gmail.com>
2019-01-03 17:54:57 -0800

    todo item initial list

---
+++ src/TodoItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function TodoItem() {
+  return (
+    <div>
+ 
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+    </div>
+  
+    )
+  }
+
+
+
+
+
+
+
+
+export default TodoItem;
