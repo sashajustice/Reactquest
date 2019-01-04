dc056c4babe61f6a8b20fd2f6648104b9c7c8853
sashajustice <sabrinnasreddin@gmail.com>
2019-01-03 20:07:42 -0800

    Add styling to list component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-// import './App.css';
+import './Style.css';
 import TodoItem from './TodoItem';
+
+
+
 
 function App() {
   return (
-    <div>
+    <div className="todo-list">
       <TodoItem /> 
       <TodoItem />
       <TodoItem /> 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TodoItem() {
   return (
-    <div>
+    <div className="todo-item">
  
       <input type="checkbox" />
       <p>Placeholder text here</p>
